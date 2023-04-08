99a819b108e1670bad1ab5ad0343d1bb3ba05986
satheeshpolu <128940409+satheeshpolu@users.noreply.github.com>
2023-04-08 11:10:25 +0200

    Splash screen (#5)
    * enhancement: added assets directory
    
    * feat: added splash screen

---
+++ src/screens/splash/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    StatusBar,
+    Text,
+    View,
+} from 'react-native';
+
+import styles from './styles';
+
+const SplashScreen = (): JSX.Element => {
+    const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    return (
+        <View style={styles.container}>
+            <StatusBar hidden={true} />
+            <Text style={styles.text}>{currentTime}</Text>
+            <Text style={styles.text}>Splash Screen</Text>
+        </View>
+    )
+}
+
+export default SplashScreen;
---
+++ src/screens/splash/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:
+            'center',
+        backgroundColor:  '#0000CC'
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 50,
+        color: '#D6D6FF'
+    }
+});
+
+export default styles;
