358460e0ed8876a10a449a8cb4b3f6a77e5ceb72
satheeshpolu <128940409+satheeshpolu@users.noreply.github.com>
2023-04-08 11:43:49 +0200

    feat: added home screen (#6)

---
+++ src/screens/home/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:
+            'center',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 50,
+        color: '#010128'
+    }
+});
+
+export default styles;
