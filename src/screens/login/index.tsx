6a55ea96362b51bb8453604fa7842bddc1c09033
satheeshpolu <satheeshpoluab@gmail.com>
2023-04-10 13:01:03 +0200

    fix: after ran lint/prettier issues are fixed

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import {
-    Button,
-    View,
-} from 'react-native';
+import { Button, View } from 'react-native';
 
 import styles from './styles';
 import { NavigationScreenProp } from 'react-navigation';
 
 type ScreenProps = {
-    navigation: NavigationScreenProp<any, any>;
-}
+  navigation: NavigationScreenProp<any, any>;
+};
 
 const LoginScreen = ({ navigation }: ScreenProps) => {
-    const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    return (
-        <View style={styles.container}>
-            <Button
-                title="Go to Home"
-                onPress={() =>
-                    navigation.navigate('Home')
-                }
-            />
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    </View>
+  );
+};
 
 export default LoginScreen;
