f862282b54915ff3f1408ed35550304d39fb431c
satheeshpolu <satheeshpoluab@gmail.com>
2023-05-13 21:53:35 +0200

    feat: support for react-native-vector-icons

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,6 +3,7 @@ import { Button, Text, View } from 'react-native';
 
 import styles from './styles';
 import { NavigationProp } from '@react-navigation/native';
+import { Avatar } from 'react-native-paper';
 
 type ScreenProps = {
   navigation: NavigationProp<any, any>;
@@ -16,6 +17,7 @@ const HomeScreen = ({ navigation }: ScreenProps): JSX.Element => {
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
       <Button title="Go To Login" onPress={sendTo} />
+      <Avatar.Icon size={50} icon="folder" />
     </View>
   );
 };
