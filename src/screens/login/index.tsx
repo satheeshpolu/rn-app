723e4e9e393590a957ed924e9c2c5188b3a655c5
satheeshpolu <satheeshpoluab@gmail.com>
2023-09-10 15:29:37 +0200

    feat: BE integratoin

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -3,15 +3,45 @@ import { Button, View } from 'react-native';
 
 import styles from './styles';
 import { NavigationScreenProp } from 'react-navigation';
+import { TextInput } from 'react-native-paper';
+import axios from 'axios';
 
 type ScreenProps = {
   navigation: NavigationScreenProp<any, any>;
 };
 
 const LoginScreen = ({ navigation }: ScreenProps) => {
+  const [title, setTitle] = React.useState('');
+  const [desc, setDesc] = React.useState('');
+
+  const saveNotes = async () => {
+    await axios
+      .post('api/v1/add-note', {
+        note_title: title,
+        note_description: desc,
+      })
+      .then(function (response) {
+        console.log('res => ', response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <TextInput
+        label="Title"
+        value={title}
+        onChangeText={text => setTitle(text)}
+      />
+      <TextInput
+        label="Description"
+        value={desc}
+        onChangeText={text => setDesc(text)}
+      />
+      <Button title="Save Notes" onPress={saveNotes} />
     </View>
   );
 };
