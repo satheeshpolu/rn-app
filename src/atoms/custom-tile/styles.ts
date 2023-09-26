618ce13f4f512b354e12d2c8b2487402c570cfbf
satheeshpolu <satheeshpoluab@gmail.com>
2023-09-26 05:31:16 +0200

    feat: created new CustomTile component

---
+++ src/atoms/custom-tile/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 2,
+    width: '40%',
+    justifyContent: 'center',
+    margin: 5,
+    borderWidth: 1,
+    borderColor: '#a3a7a4',
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 24,
+    color: '#424442'
+  },
+  icon: {
+   padding: 5,
+ },
+  subTitle: {
+    fontSize: 16,
+    color: '#555856'
+  },  
+});
+
+export default styles;
